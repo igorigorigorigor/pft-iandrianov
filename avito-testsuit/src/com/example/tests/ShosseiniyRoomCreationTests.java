6b88062d758e75d841d730e505645ab73b3e3831
andrianov2406 <andrianov2406@gmail.com>
2014-08-18 10:07:47 +0400

    Added ShosseiniyRoom classes

---
+++ avito-testsuit/src/com/example/tests/ShosseiniyRoomCreationTests.java
@@ -0,0 +1,32 @@
+package com.example.tests;
+
+import static com.example.tests.ApartmentDataGenerator.loadApartmentFromXmlFile;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.example.fw.Apartment;
+
+import java.io.*;
+import java.util.Iterator;
+
+
+public class ShosseiniyRoomCreationTests extends TestBase {
+
+	@DataProvider
+	public Iterator<Object[]> apartmentFromFile() throws IOException {
+		return wrapApartmentForDataProvider(loadApartmentFromXmlFile(new File("ShosseiniyRoom.xml"))).iterator();
+	}
+	
+	@Test(dataProvider = "apartmentFromFile")
+	public void testApartmentCreationWithValidData(Apartment flat) throws Exception {
+		
+		//sign in
+		app.getapartmenthelper().signin();
+		
+		//actions
+		app.getapartmenthelper().createApartment(flat);
+		
+	}
+}
+
